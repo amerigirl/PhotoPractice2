ba88028a4ffd39f2ad6142c5963e9d871fc15002
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-12-02 20:43:06 -0800

    ready to test

--- app/src/main/java/com/example/finalphoto2/MainActivity.kt
+++ app/src/main/java/com/example/finalphoto2/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.finalphoto2
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var our_request_code: Int = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun takePhoto(view: View) {
+        //start an intent to capture image
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        //start the result
+
+         //check if the task can be performed or not
+        if(intent.resolveActivity(packageManager) !==null){
+            startActivityForResult(intent, our_request_code)
+        }
+    }
+
+    //now onactivity
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == our_request_code && resultCode == RESULT_OK){
+
+            //if result ok and equal to request code
+
+            val imageView : ImageView = findViewById((R.id.image))
+            //start bitmap
+            val bitmap = data?.extras?.get("data") as Bitmap
+            //set image bitmap
+            imageView.setImageBitmap(bitmap)
+
         }
     }
 }
